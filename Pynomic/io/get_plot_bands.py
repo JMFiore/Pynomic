53921a827335e9e9c5c0387c86a56aa604c1de19
Juan Manuel Fiore <97068559+JMFiore@users.noreply.github.com>
2024-10-13 11:22:36 -0300

    Add files via upload

--- Pynomic/io/get_plot_bands.py
+++ Pynomic/io/get_plot_bands.py
@@ -13,14 +13,15 @@ import numpy as np
 import pandas as pd
 import rasterio
 import cv2
-from scipy import ndimage
-import math
 import os
 import json
 from shapely.geometry import MultiPolygon, Polygon
 from rasterio import mask
 from Pynomic.core import core
 import re
+from PIL import Image
+import zarr
+import io
 
 # =============================================================================
 # FUNCTIONS
@@ -72,81 +73,50 @@ def _get_tiff_files(fold_path):
     return tiff_list
 
 
-def auto_fit_image(image, rot90=True, rimage=False, hbuffer=2, wbuffer=2):
-    """Get's The parameters croop and angle automaticaly.
+def auto_fit_image(mtx, hbuffer=2, wbuffer=2):
+    """Takes an array and returns the crooping and angle parameters.
 
-    Args:
-        Image: an RGB Image or an array.
-        rot90: boolean default True.
-        rimage: rotation angle if wanted.
-        hbuffer: number of pixels to buffer the image horizontaly.
-        wbuffer: number of pixels to buffer the image horizontaly.
+    Args
+        mtx: np.array
+        hbuffer: int. height buffer.
+        wbuffer: int. with buffer.
 
     Returns
     -------
-        an image or parameters size of array to crop and angle to rotate.
+        tuple with croping parameters. Angle value.
     """
-    # Gets the edges and generates a line to calculate the angle
-    if isinstance(image, np.ndarray):
-        gray = image
-    else:
-        gray = np.array(image.convert("L"))
+    gray = np.uint8(np.where(mtx <= 0, mtx, 255)).copy()
+    gray = np.uint8(np.where(gray > 0, gray, 0))
+    gray = np.array(gray)
 
-    img_edges = cv2.Canny(gray, 100, 100, apertureSize=3)
-    lines = cv2.HoughLinesP(
-        img_edges, 1, math.pi / 180.0, 100, minLineLength=100, maxLineGap=10
+    # edges = cv2.Canny(gray,150,150*2)
+
+    contours, hierarchy = cv2.findContours(
+        gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
 
-    # Calculates the angle
-    angles = []
-    for [[x1, y1, x2, y2]] in lines:
-        angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
-        angles.append(angle)
+    rect = cv2.minAreaRect(contours[-1])
 
-    angle = np.median(angles)
-
-    # Applies the angle to the image and transforms it to horizonatal or
-    # vertical
-    if rot90 is True:
-        rangle = 90 + angle
-
-        if isinstance(image, np.ndarray):
-            rot_im = ndimage.rotate(image.copy(), rangle)
-        else:
-            rot_im = image.copy().rotate(rangle, expand=True)
-
+    if rect[1][0] < rect[1][1]:
+        angle = rect[2]
+        gry = np.array(Image.fromarray(gray).rotate(angle, expand=True))
     else:
-        rangle = angle
-        if isinstance(image, np.ndarray):
-            rot_im = ndimage.rotate(image.copy(), rangle)
-        else:
-            rot_im = image.copy().rotate(rangle, expand=True)
-
-    # Gets the crooping parameters to eliminate the black background.
-    if isinstance(image, np.ndarray):
-        gry = rot_im
-    else:
-        gry = np.array(rot_im.convert("L"))
+        angle = rect[2] + 90
+        gry = np.array(Image.fromarray(gray).rotate(angle, expand=True))
 
     blur = cv2.GaussianBlur(gry, (3, 3), 0)
     th = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     coords = cv2.findNonZero(th)
     x, y, w, h = cv2.boundingRect(coords)
 
-    if rimage:
-        if isinstance(image, np.ndarray):
-            return rot_im[
-                (y + hbuffer) : (h - hbuffer), (x + wbuffer) : (w - wbuffer)
-            ]
-        else:
-            return rot_im.crop(
-                (x + wbuffer, y + hbuffer, w - wbuffer, h - hbuffer)
-            )
-    else:
-        return (x + wbuffer, y + hbuffer, w - wbuffer, h - hbuffer), rangle
+    h1 = y + hbuffer
+    h2 = y + (h - hbuffer)
+    w1 = x + wbuffer
+    w2 = x + (w - wbuffer)
+    return (h1, h2, w1, w2), angle
 
 
-def _extract_bands_from_raster(raster_data, multiplot):
+def _extract_bands_from_raster(raster_data, multiplot, alpha_idx=-1):
     """Separates the bands for the maks.
 
     Args:
@@ -161,8 +131,12 @@ def _extract_bands_from_raster(raster_data, multiplot):
         raster_data, multiplot.geoms, crop=True
     )
 
-    true_bands = masked_rast[:-1]
-    masked_band = masked_rast[-1]
+    if alpha_idx == -1:
+        true_bands = masked_rast.copy()
+        masked_band = masked_rast[-1]
+    else:
+        true_bands = list(masked_rast)
+        masked_band = true_bands.pop(alpha_idx)
 
     return true_bands, masked_band
 
@@ -181,21 +155,33 @@ def extract_raster_data(raster_path, grid_path, bands_n=None):
         DataFrame with date, mean band for each plot.
         list bands name.
     """
-    grid_crs, grids = _read_grid(grid_path)
+    grid_cs, grids = _read_grid(grid_path)
     bands_mean = []
     array_dict = {}
     bands_name = []
-    with rasterio.open(raster_path) as crs:
+    with rasterio.open(raster_path) as src:
         for pos, g in enumerate(grids.keys()):
             if int(pos) == 0:
-                coords = crs.meta["crs"]
+                coords = src.meta["crs"]
                 print(f"Raster Coords system: {coords}")
-                print(f"Grid Coords system: {grid_crs}")
+                print(f"Grid Coords system: {grid_cs}")
 
-            # Diferentiate the true bands form the mask band.
-            true_bands, masked_band = _extract_bands_from_raster(
-                crs, MultiPolygon([Polygon(grids[g])])
-            )
+            # Check if contains alpha band
+            contains_alpha_band = -1
+            for idx, interp in enumerate(src.colorinterp, start=1):
+                if interp == rasterio.enums.ColorInterp.alpha:
+                    contains_alpha_band = idx - 1
+
+            if contains_alpha_band != -1:
+                # Diferentiate the true bands form the mask band.
+                true_bands, masked_band = _extract_bands_from_raster(
+                    src, MultiPolygon([Polygon(grids[g])]), contains_alpha_band
+                )
+            else:
+                # Returns the las band for fiting.
+                true_bands, masked_band = _extract_bands_from_raster(
+                    src, MultiPolygon([Polygon(grids[g])])
+                )
 
             # Get the fitting parameters.
             cpv, rangle = auto_fit_image(masked_band)
@@ -209,7 +195,9 @@ def extract_raster_data(raster_path, grid_path, bands_n=None):
 
             # Fit the bands array with the parameters.
             fitted_bands = [
-                ndimage.rotate(band, rangle)[cpv[1] : cpv[3], cpv[0] : cpv[2]]
+                np.array(Image.fromarray(band).rotate(rangle, expand=True))[
+                    cpv[0] : cpv[1], cpv[2] : cpv[3]
+                ]
                 for band in true_bands
             ]
 
@@ -218,7 +206,11 @@ def extract_raster_data(raster_path, grid_path, bands_n=None):
                 np.mean(band.astype(float)) for band in fitted_bands
             ]
             mp_bands = []
+            # Numerical id for project.
+            mp_bands.append(pos + 1)
+            # Original id from the grid can be numerical or text or both.
             mp_bands.append(g)
+            # gets the date
             mp_bands.append(os.path.basename(raster_path).split("_")[0])
             for band in plot_fitted_bands:
                 mp_bands.append(band)
@@ -227,7 +219,9 @@ def extract_raster_data(raster_path, grid_path, bands_n=None):
             # Save the values in a dictionary.
             array_dict[g] = dict(zip(bands_name, fitted_bands))
 
-    df = pd.DataFrame(bands_mean, columns=["id", "date", *bands_name])
+    df = pd.DataFrame(
+        bands_mean, columns=["id", "original_id", "date", *bands_name]
+    )
     return array_dict, bands_name, df
 
 
@@ -244,24 +238,67 @@ def process_stack_tiff(folder_path, grid_path, bands_n=None):
         PynomicsProject object.
     """
     tif_list = _get_tiff_files(folder_path)
-    features_dict = {}
+    raw_data = zarr.group()
+    raw_data.create_group("dates")
     dates = []
     ldata = []
+    date_key = ""
     for tiff_pos, tiff_file in enumerate(tif_list):
         print(f"{tiff_pos + 1}/{len(tif_list)} : {tiff_file}")
         if re.search(r"_", tiff_file):
-            dates.append(tiff_file.split("_")[0])
+            date_key = tiff_file.split("_")[0]
+            dates.append(date_key)
+
         to_raw_data, bands_n, ldata_bands = extract_raster_data(
             folder_path + "/" + tiff_file, grid_path, bands_n
         )
-        features_dict[tiff_file] = to_raw_data
+
+        raw_data["dates"].create_group(date_key)
+
+        for plot_id in to_raw_data.keys():
+            raw_data["dates"][date_key].create_group(plot_id)
+            for band in to_raw_data[plot_id].keys():
+                raw_data["dates"][date_key][plot_id].create_group(band)
+                raw_data["dates"][date_key][plot_id][band] = to_raw_data[
+                    plot_id
+                ][band]
+
         ldata.append(ldata_bands)
+
     df_data = pd.concat(ldata, axis=0)
+
     return core.Pynomicproject(
-        raw_data=features_dict,
+        raw_data=raw_data,
         ldata=df_data,
         dates=dates,
         n_dates=len(dates),
         bands_name=bands_n,
         n_bands=len(bands_n),
+    )
+
+
+def read_zarr(path):
+    """Reads a zarr project previously saved from pynomics.
+
+    Args
+        path: a path to the zarr folder
+
+    Returns
+    -------
+        Pynomicproject object
+    """
+    store = zarr.open(path, mode='a')
+    info = zarr.group()
+    zarr.copy_all(store, info)
+    df_buffer = io.BytesIO()
+    df_buffer.write(info["ldata"][0])
+    ldata = pd.read_parquet(df_buffer)
+
+    return core.Pynomicproject(
+        raw_data=info,
+        ldata=ldata.copy(),
+        n_dates=len(ldata.date.unique()),
+        dates=ldata.date.unique(),
+        n_bands=len(info.bands_name[:]),
+        bands_name=info.bands_name[:].copy(),
     )
