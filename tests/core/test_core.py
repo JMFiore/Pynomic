1f9763c5f4edaeff07b2d45a1a9e2299e7389726
Juan Manuel Fiore <97068559+JMFiore@users.noreply.github.com>
2024-10-26 17:41:47 -0300

    Add files via upload

--- tests/core/test_core.py
+++ tests/core/test_core.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 # License: MIT
-# Copyright (c) 2024,  Fiore J.Manuel
+# Copyright (c) 2024, Fiore J.Manuel.
 # All rights reserved.
 
 # =============================================================================
@@ -9,42 +9,46 @@
 # =============================================================================
 from Pynomic.core import core
 from Pynomic.io import get_plot_bands
+from Pynomic.io.get_plot_bands import read_zarr
 
 import numpy as np
 
 import pandas as pd
 
-
 import pytest
+
+import zarr
 
 # =============================================================================
 # FUNCTIONS
 # =============================================================================
 
 
-def test_Pnomicporject_obj():
+def test_Pynomicporject_obj():
     pyt = get_plot_bands.process_stack_tiff(
         "add_on/flights",
         "add_on/Grids/Labmert_test_grid.geojson",
+        "fid",
         ["red", "green", "blue"],
     )
 
     assert isinstance(pyt, core.Pynomicproject)
 
     assert isinstance(pyt.bands_name, list)
-    assert isinstance(pyt.raw_data, dict)
+    assert isinstance(pyt.raw_data, zarr.hierarchy.Group)
     assert isinstance(pyt.ldata, pd.DataFrame)
     assert isinstance(pyt.dates, list)
     assert isinstance(pyt.n_bands, int)
     assert isinstance(pyt.n_dates, int)
 
     assert pyt.n_dates == 7
-    assert pyt.ldata.shape == (280, 5)
+    assert pyt.ldata.shape == (280, 7)
     assert pyt.ldata.columns[0] == "id"
-    assert pyt.ldata.columns[1] == "date"
-    assert pyt.ldata.columns[2] == "red"
-    assert pyt.ldata.columns[3] == "green"
-    assert pyt.ldata.columns[4] == "blue"
+    assert pyt.ldata.columns[1] == "fid"
+    assert pyt.ldata.columns[2] == "date"
+    assert pyt.ldata.columns[3] == "red"
+    assert pyt.ldata.columns[4] == "green"
+    assert pyt.ldata.columns[5] == "blue"
     assert pyt.dates[0] == "20180815"
     assert pyt.dates[1] == "20180917"
     assert pyt.dates[2] == "20180905"
@@ -62,6 +66,7 @@ def test_getitem():
     pyt = get_plot_bands.process_stack_tiff(
         "add_on/flights",
         "add_on/Grids/Labmert_test_grid.geojson",
+        "fid",
         ["red", "green", "blue"],
     )
 
@@ -77,6 +82,7 @@ def test_generate_unique_feature():
     pyt = get_plot_bands.process_stack_tiff(
         "add_on/flights",
         "add_on/Grids/Labmert_test_grid.geojson",
+        "fid",
         ["red", "green", "blue"],
     )
 
@@ -85,18 +91,138 @@ def test_generate_unique_feature():
         green = np.mean(df["green"])
         blue = np.mean(df["blue"])
 
-        return (2 * green - red - blue) / (2 * green + red + blue)
+        return [(2 * green - red - blue) / (2 * green + red + blue)]
 
-    data = pyt.generate_unique_feature(VDVI_index, "VDVI")
+    data = pyt.generate_unique_feature(VDVI_index, ["VDVI"])
 
     assert isinstance(data, pd.DataFrame)
 
-    assert data.loc[0, "VDVI"] == 0.16568972306249757
-    assert data.loc[3, "VDVI"] == 0.1308294903034819
-    assert data.loc[275, "VDVI"] == 0.09725847938267855
-    assert pyt.ldata.shape[1] == 5
+    assert data.loc[0, "VDVI"] == 0.1546743079777065
+    assert data.loc[3, "VDVI"] == 0.1554692596154336
+    assert data.loc[275, "VDVI"] == 0.1278593224337763
+    assert pyt.ldata.shape[1] == 7
 
-    pyt.generate_unique_feature(VDVI_index, "VDVI", to_data=True)
-    assert pyt.ldata.shape[1] == 6
+    pyt.generate_unique_feature(VDVI_index, ["VDVI"], to_data=True)
+    assert pyt.ldata.shape[1] == 8
+
+    return
+
+
+def test_senescence_prediction():
+    pyt = get_plot_bands.process_stack_tiff(
+        "add_on/flights",
+        "add_on/Grids/Labmert_test_grid.geojson",
+        "fid",
+        ["red", "green", "blue"],
+    )
+
+    def VDVI_index(df):
+        red = np.mean(df["red"])
+        green = np.mean(df["green"])
+        blue = np.mean(df["blue"])
+
+        return [(2 * green - red - blue) / (2 * green + red + blue)]
+
+    pyt.generate_unique_feature(VDVI_index, ["VDVI"], True)
+
+    df1 = pyt.get_senescens_predictions("VDVI", 0.1)
+
+    assert int(df1.loc[df1.id == 1, "dpred"].values[0]) == 13
+    assert int(df1.loc[df1.id == 2, "dpred"].values[0]) == 16
+    assert float(df1.loc[df1.id == 8, "dpred"].values[0]) == -4.0
+    assert float(df1.loc[df1.id == 35, "dpred"].values[0]) == -10.0
+
+    pyt.get_senescens_predictions("VDVI", 0.001, True)
+    df1 = pyt.ldata
+
+    assert int(df1.loc[df1.id == 1, "dpred"].values[0]) == -260
+    assert int(df1.loc[df1.id == 2, "dpred"].values[0]) == 1283
+
+    return
+
+
+def test_save_fun():
+    pyt = get_plot_bands.process_stack_tiff(
+        "add_on/flights",
+        "add_on/Grids/Labmert_test_grid.geojson",
+        "fid",
+        ["red", "green", "blue"],
+    )
+    pyt.save("add_on/zarr_data/RGB_group.zip")
+    pyt1 = read_zarr("add_on/zarr_data/RGB_group.zip")
+    pyt1.save("add_on/zarr_data/RGB_group.zip")
+    pyt2 = read_zarr("add_on/zarr_data/RGB_group.zip")
+
+    assert pyt1.bands_name[0] == "red"
+    assert pyt1.bands_name[1] == "green"
+    assert pyt1.bands_name[2] == "blue"
+
+    assert isinstance(pyt1.bands_name, list)
+    assert isinstance(pyt1.raw_data, zarr.hierarchy.Group)
+    assert isinstance(pyt1.ldata, pd.DataFrame)
+    assert isinstance(pyt1.dates, list)
+    assert isinstance(pyt1.n_bands, int)
+    assert isinstance(pyt1.n_dates, int)
+
+    assert pyt1.n_dates == 7
+    assert pyt1.ldata.shape == (280, 7)
+    assert pyt1.ldata.columns[0] == "id"
+    assert pyt1.ldata.columns[1] == "fid"
+    assert pyt1.ldata.columns[2] == "date"
+    assert pyt1.ldata.columns[3] == "red"
+    assert pyt1.ldata.columns[4] == "green"
+    assert pyt1.ldata.columns[5] == "blue"
+
+    assert pyt2.bands_name[0] == "red"
+    assert pyt2.bands_name[1] == "green"
+    assert pyt2.bands_name[2] == "blue"
+
+    assert isinstance(pyt2.bands_name, list)
+    assert isinstance(pyt2.raw_data, zarr.hierarchy.Group)
+    assert isinstance(pyt2.ldata, pd.DataFrame)
+    assert isinstance(pyt2.dates, list)
+    assert isinstance(pyt2.n_bands, int)
+    assert isinstance(pyt2.n_dates, int)
+
+    assert pyt2.n_dates == 7
+    assert pyt2.ldata.shape == (280, 7)
+    assert pyt2.ldata.columns[0] == "id"
+    assert pyt2.ldata.columns[1] == "fid"
+    assert pyt2.ldata.columns[2] == "date"
+    assert pyt2.ldata.columns[3] == "red"
+    assert pyt2.ldata.columns[4] == "green"
+    assert pyt2.ldata.columns[5] == "blue"
+
+    return
+
+
+def test_RGB_ind():
+    pyt = get_plot_bands.process_stack_tiff(
+        "add_on/flights",
+        "add_on/Grids/Labmert_test_grid.geojson",
+        "fid",
+        ["red", "green", "blue"],
+    )
+
+    pyt.RGB_VI(Red="red", Blue="blue", Green="green")
+
+    assert pyt.ldata.shape[0] == 280
+    assert pyt.ldata.shape[1] == 17
+
+    assert "VDVI" in pyt.ldata.columns
+    assert "NGRDI" in pyt.ldata.columns
+    assert "VARI" in pyt.ldata.columns
+    assert "GRRI" in pyt.ldata.columns
+    assert "VEG" in pyt.ldata.columns
+    assert "MGRVI" in pyt.ldata.columns
+    assert "GLI" in pyt.ldata.columns
+    assert "ExB" in pyt.ldata.columns
+    assert "ExG" in pyt.ldata.columns
+    assert "ExR" in pyt.ldata.columns
+
+    assert (
+        pyt.ldata.loc[pyt.ldata.id == 1, "VDVI"].values[0]
+        == 0.1546743079777065
+    )
 
     return
---
+++ tests/core/test_plot.py
@@ -0,0 +1,94 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# License: MIT
+# Copyright (c) 2024, Fiore J.Manuel.
+# All rights reserved.
+
+"""Provides the objects and functions."""
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+from Pynomic.io import get_plot_bands
+
+
+import numpy as np
+
+# =============================================================================
+# CLASSES
+# =============================================================================
+
+
+def test_image_timeline():
+
+    pyt = get_plot_bands.process_stack_tiff(
+        "add_on/flights",
+        "add_on/Grids/Labmert_test_grid.geojson",
+        "fid",
+        ["red", "green", "blue"],
+    )
+
+    def VDVI_inex(df):
+        red = np.mean(df["red"])
+        green = np.mean(df["green"])
+        blue = np.mean(df["blue"])
+
+        return [(2 * green - red - blue) / (2 * green + red + blue)]
+
+    def VDVI_inex2(df):
+        red = np.float16(df["red"])
+        green = np.float16(df["green"])
+        blue = np.float16(df["blue"])
+
+        return (2 * green - red - blue) / (2 * green + red + blue)
+
+    pyt.generate_unique_feature(VDVI_inex, ["VDVI"], to_data=True)
+
+    at, atb = pyt.plot.image_timeline("VDVI", "3", VDVI_inex2)
+
+    assert atb.get_xlabel() == "Flight dates"
+    assert atb.get_title() == ""
+    assert atb.get_ylabel() == "VDVI"
+    assert atb.get_yscale() == "linear"
+    # assert atb.get_yticks()[1] == np.float64(0.025)
+    # assert atb.get_xticks()[1] == np.float64(17765.0)
+    # assert atb.get_ylim()[1] == np.float64(0.1723043510875103)
+    # assert atb.get_xlim()[1] == np.float64(17801.05)
+
+    atd, atbd = pyt.plot.image_timeline("VDVI", "3", VDVI_inex2, days=True)
+    assert atbd.get_xlabel() == "Flight days"
+    return
+
+
+def test_timeline():
+
+    pyt = get_plot_bands.process_stack_tiff(
+        "add_on/flights",
+        "add_on/Grids/Labmert_test_grid.geojson",
+        "fid",
+        ["red", "green", "blue"],
+    )
+
+    def VDVI_inex(df):
+        red = np.mean(df["red"])
+        green = np.mean(df["green"])
+        blue = np.mean(df["blue"])
+
+        return [(2 * green - red - blue) / (2 * green + red + blue)]
+
+    pyt.generate_unique_feature(VDVI_inex, ["VDVI"], to_data=True)
+
+    axis = pyt.plot.timeline("VDVI", "3")
+
+    assert axis.get_xlabel() == "Flight dates"
+    assert axis.get_title() == "VDVI - Plot Id 3"
+    assert axis.get_ylabel() == "VDVI"
+    assert axis.get_yscale() == "linear"
+    # assert axis.get_yticks()[1] == np.float64(-0.05)
+    # assert axis.get_xticks()[1] == np.float64(17765.0)
+    # assert axis.get_ylim()[0] == np.float64(0.00018280194085929305)
+    # assert axis.get_xlim()[0] == np.float64(-889.95)
+
+    axis_days = pyt.plot.timeline("VDVI", "3", days=True)
+    assert axis_days.get_xlabel() == "Flight days"
+    return
