4ad2b7e0852f39db6500a1a5c37e5e7d9f9b41d0
Juan Manuel Fiore <97068559+JMFiore@users.noreply.github.com>
2024-10-26 17:39:32 -0300

    Add files via upload

--- Pynomic/__init__.py
+++ Pynomic/__init__.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 # License: MIT
-# Copyright (c) 2024, Fiore J.Manuel
+# Copyright (c) 2024, Fiore J.Manuel.
 # All rights reserved.
 
 # =============================================================================
@@ -28,5 +28,7 @@ __version__ = "0.0.1"
 from .core import Pynomicproject
 from .io.get_plot_bands import process_stack_tiff
 from .io.get_plot_bands import auto_fit_image
+from .io.get_plot_bands import read_zarr
 
-__all__ = ["Pynomicproject", "process_stack_tiff", "auto_fit_image"]
+__all__ = ["Pynomicproject", "process_stack_tiff",
+           "auto_fit_image", "read_zarr"]
